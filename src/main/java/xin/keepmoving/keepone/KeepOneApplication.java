5810a49683e1ee0d43592b876c422427a5a97b7a
nirvanafire <nirvanafire@gmail.com>
2020-05-08 08:01:27 +0800

    init

---
+++ src/main/java/xin/keepmoving/keepone/KeepOneApplication.java
@@ -0,0 +1,13 @@
+package xin.keepmoving.keepone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepOneApplication.class, args);
+    }
+
+}
