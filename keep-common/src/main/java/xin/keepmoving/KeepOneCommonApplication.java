483f412c197b8cbc8163514de670524efecee5b1
nirvanafire <nirvanafire@gmail.com>
2020-05-25 22:15:47 +0800

    mybatis plus

---
+++ keep-common/src/main/java/xin/keepmoving/KeepOneCommonApplication.java
@@ -0,0 +1,17 @@
+package xin.keepmoving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ * @author Nirvana Fire
+ */
+@SpringBootApplication
+public class KeepOneCommonApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(KeepOneCommonApplication.class, args);
+    }
+}
---
+++ keep-common/src/test/java/xin/keepmoving/KeepOneCommonApplicationTest.java
@@ -0,0 +1,20 @@
+package xin.keepmoving;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class KeepOneCommonApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
